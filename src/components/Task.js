207a794cd8d79381a90ae14731560aa2970ec9fb
Yngve Molnes <yngve.molnes@gmail.com>
2018-10-19 22:15:22 +0200

    Added maps to tasks, and tasks generally works, woho

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,44 +4,67 @@ import Icon from 'react-native-vector-icons/Ionicons';
 
 
 export default class Task extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            doneColor: props.val.isDone ? 'forestgreen' : 'limegreen'
+        }
+    }
     render() {
         return (
             <View key={this.props.keyval} style={styles.task}>
-                <Text style={styles.taskText}>{this.props.val.date}</Text>
-                <Text style={styles.taskText}>{this.props.val.task}</Text>
-                <TouchableOpacity onPress={this.props.deleteMethod} style={styles.taskDelete}>
-                    <Icon name="md-remove-circle" style={styles.actionButtonIcon}/>
-                </TouchableOpacity>
+                <View style={styles.text}>
+                    <Text style={styles.taskDate}>{this.props.val.date}</Text>
+                    <Text style={styles.taskText}>{this.props.val.task}</Text>
+                </View>
+                <View style={styles.buttons}>
+                    <TouchableOpacity onPress={this.props.deleteMethod} style={styles.taskDelete}>
+                        <Icon name="md-remove-circle" style={styles.actionButtonIconDelete} />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={this.props.completeMethod} style={styles.taskDelete}>
+                        <Icon name="md-checkmark-circle" style={styles.actionButtonIconCheck} />
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
 }
 const styles = StyleSheet.create({
     task: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
         position: 'relative',
         padding: 20,
-        paddingRight: 100,
         backgroundColor: 'white',
         borderBottomWidth:2,
         borderBottomColor: '#ededed'
     },
+    text: {
+
+    },
+    buttons: {
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    taskDate: {
+        color: '#bbb'
+    },
     taskText: {
-        paddingLeft: 0,
-        borderLeftWidth: 0,
-        color: 'black'
+        fontSize: 19,
+        overflow: 'hidden'
     },
     taskDelete: {
-        position: 'absolute',
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 20,
-        top: 10,
-        bottom: 10,
-        right: 0
     },
-    actionButtonIcon: {
+    actionButtonIconDelete: {
         fontSize: 40,
         height: 40,
-        color: 'slateblue'
-    }
+        color: 'red'
+    },
+    actionButtonIconCheck: {
+        fontSize: 40,
+        height: 40,
+        color: 'forestgreen',
+        marginLeft: 12
+    },
 });
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Text, TextInput, StyleSheet, Button, TouchableHighlight, View, ScrollView, Modal, Alert } from 'react-native';
-import ActionButton from 'react-native-action-button';
-import Icon from 'react-native-vector-icons/Ionicons';
+import { Text, TextInput, StyleSheet, Button, View, ScrollView, Modal, AsyncStorage } from 'react-native';
+import MapView from 'react-native-maps'
+import { Marker } from 'react-native-maps'
+import ActionButton from 'react-native-action-button'
 import Task from './Task';
 
 export default class Header extends React.Component {
@@ -11,26 +12,48 @@ export default class Header extends React.Component {
         this.state = {
             taskArray: [],
             taskText: '',
-            modalVisible: false
+            modalVisible: false,
+            savePos: false,
+            latitude: 37,
+            longitude: -122,
+            error: null,
+            marker: null
         };
+        this.getTasksFromStorage();
     }
 
     setModalVisible(visible) {
         this.setState({modalVisible: visible});
     }
 
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                    error: null
+                })
+            },
+            error => this.setState({ error: error.message }),
+            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+        )
+    }
+
     render() {
         let tasks = this.state.taskArray.map((val, key)=>{
             return <Task key={key} keyval={key} val={val}
-                    deleteMethod={()=>this.deleteTask(key)}/>
+                        completeMethod={() => this.completeTask(key)}
+                        deleteMethod={() => this.deleteTask(key)}
+                    />
         });
 
         return (
             <View style={styles.container}>
                 <Modal
-                animationType="slide"
-                transparent={false}
-                visible={this.state.modalVisible}
+                    animationType="slide"
+                    transparent={false}
+                    visible={this.state.modalVisible}
                 >
                     <View style={{marginTop: 50}}>
                         <Button 
@@ -38,10 +61,32 @@ export default class Header extends React.Component {
                             onPress={() => { this.setModalVisible(false);}}
                         />
                         <TextInput
-                        style={styles.textInput}
-                        placeholder='YOLO'
-                        onChangeText={(taskText)=> this.setState({taskText})}
-                        value={this.state.taskText}
+                            style={styles.textInput}
+                            placeholder='YOLO'
+                            onChangeText={(taskText)=> this.setState({taskText})}
+                            value={this.state.taskText}
+                        />
+                        {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+                        <MapView
+                            style={{height: 300}}
+                            initialRegion={{
+                                latitude: this.state.latitude,
+                                longitude: this.state.longitude,
+                                latitudeDelta: 0.0922,
+                                longitudeDelta: 0.0421
+                            }}
+                        >
+                            {
+                                this.state.marker ?
+                                <Marker
+                                    coordinate={this.state.marker.latlng}
+                                />
+                                : null
+                            }
+                        </MapView>
+                        <Button
+                            title="Add current location"
+                            onPress={this.addLocation.bind(this)}
                         />
                         <Button
                             title='Save'
@@ -49,11 +94,14 @@ export default class Header extends React.Component {
                         />
                     </View>
                 </Modal>
+
                 <ScrollView style={styles.scrollContainer}>
                     {tasks}
                 </ScrollView>
 
-                <ActionButton buttonColor='slateblue' onPress={() => { this.setModalVisible(true);}}/>
+                <ActionButton buttonColor='slateblue' onPress={() => { 
+                    this.setModalVisible(true);
+                }}/>
             </View>
         );
     }
@@ -64,16 +112,61 @@ export default class Header extends React.Component {
                 'date':d.getFullYear()+
                 "/"+(d.getMonth()+1) +
                 "/"+ d.getDate(),
-                'task': this.state.taskText
+                'task': this.state.taskText,
+                'isDone': false,
+                'lat': this.state.savePos ? this.state.latitude : null,
+                'lng': this.state.savePos ? this.state.longitude : null
             });
             this.setState({ taskArray: this.state.taskArray });
             this.setState({taskText:''});
             this.setModalVisible(false);
         }
+        this.saveTasksToStorage(this.state.taskArray)
+    }
+    addLocation() {
+        const marker = new Marker()
+        marker.latlng = {
+            latitude: this.state.latitude,
+            longitude: this.state.longitude
+        }
+        this.setState({ savePos: true, marker })
+    }
+
+    completeTask(key){
+        this.state.taskArray[key].isDone = true;
+        this.setState({taskArray: this.state.taskArray})
+        this.saveTasksToStorage()
     }
     deleteTask(key){
         this.state.taskArray.splice(key, 1);
         this.setState({taskArray: this.state.taskArray});
+        this.saveTasksToStorage()
+    }
+
+    saveTasksToStorage(tasks) {
+        if (typeof tasks === 'undefined') tasks = this.state.taskArray
+        try {
+            AsyncStorage.setItem('tasks', JSON.stringify(tasks)).catch((err) => {
+                console.log(err)
+            })
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    async getTasksFromStorage() {
+        try {
+            AsyncStorage.getItem('tasks')
+            .then((req) => {
+                if (req != undefined) {
+                    this.setState({taskArray: JSON.parse(req)});
+                    console.log(JSON.parse(req))
+                } else {
+                    this.setState({taskArray: []});
+                }
+            })
+        } catch (err) {
+        }
     }
 }
 
