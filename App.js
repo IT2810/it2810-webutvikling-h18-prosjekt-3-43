ae1b698ce8e1755fba296d9c939c4a8d02bda2a0
Yngve Molnes <yngve.molnes@gmail.com>
2018-10-19 23:14:54 +0200

    Home page map done

--- App.js
+++ App.js
@@ -1,36 +1,95 @@
 import React from 'react';
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, AsyncStorage } from 'react-native'
 import { createDrawerNavigator, createStackNavigator } from 'react-navigation'
 import { FontAwesome } from '@expo/vector-icons'
+import MapView from 'react-native-maps'
+import { Marker } from 'react-native-maps'
 import Calendar from './src/components/Calendar'
 import Contacts from './src/components/Contacts'
 import Tasks from './src/components/Tasks'
 
 class Home extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            tasks: null,
+            initialLatitude: 37,
+            initialLongitude: -122,
+            error: null
+        }
+    }
+
     static navigationOptions = {
         title: 'Planner Home'
+    }
+
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({
+                    initialLatitude: position.coords.latitude,
+                    initialLongitude: position.coords.longitude,
+                    error: null
+                })
+            },
+            error => this.setState({ error: error.message }),
+            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+        )
+        try {
+            AsyncStorage.getItem('tasks')
+            .then((req) => {
+                if (req != undefined) {
+                    this.setState({tasks: JSON.parse(req)});
+                } else {
+                    this.setState({tasks: null});
+                }
+            })
+        } catch (err) {
+        }
     }
 
     render() {
         return (
             <View style={styles.container}>
-            <Text>Home</Text>
+                <Text style={styles.welcome}>Welcome to your very own, fantastic planner.</Text>
+                {
+                    this.state.tasks ?
+                    <View style={{ width: '100%' }}>
+                        <Text style={styles.todos}>Here is a map of your todos. Very nice.</Text>
+                        <MapView
+                            style={{ height: 300 }}
+                            initialRegion={
+                                {
+                                    latitude: this.state.initialLatitude,
+                                    longitude:  this.state.initialLongitude,
+                                    latitudeDelta: 1,
+                                    longitudeDelta: 1
+                                }
+                            }
+                        >
+                            {
+                                this.state.tasks.map((task, key) => {
+                                    return (
+                                        <Marker
+                                            key={key}
+                                            keyval={key}
+                                            coordinate={{
+                                                latitude: task.lat || 37,
+                                                longitude: task.long || -122
+                                            }}
+                                            title={task.title}
+                                        />
+                                    )
+                                })
+                            }
+                        </MapView>
+                    </View>
+                    : null
+                }
             </View>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-    },
-    actionButtonIcon: {
-        fontSize: 20,
-        height: 22,
-        color: 'white',
-    },
-});
 
 const DrawerNavigator = createDrawerNavigator(
     {
@@ -90,5 +149,21 @@ const AppNavigator = createStackNavigator(
         }),
     }
 )
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        paddingTop: 32
+    },
+    welcome: {
+        fontSize: 18
+    },
+    todos: {
+        marginTop: 24,
+        marginBottom: 8
+    }
+});
 
 export default AppNavigator
